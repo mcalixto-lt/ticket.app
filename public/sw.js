74715cc45b9215d164e015bea1dd1d8bd5ac20c3
mcalixto-lt <mcalixto.lt@gmail.com>
2026-08-14 09:47:45 -0300

    Adiciona service worker do PWA

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'ticket-app-v102';
+const CORE = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/src/styles.css',
+  '/public/manifest.webmanifest',
+  '/public/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .then((response) => {
+        if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+        return response;
+      })
+      .catch(async () => (await caches.match(event.request)) || caches.match('/index.html'))
+  );
+});
